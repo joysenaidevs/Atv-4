5b45e6e15cc5cc9f455fe31109e0411498266cb1
DEV <dev@info.local>
2020-11-30 10:19:52 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Atv_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numeros = new int [10];
+
+            for (var contador = 0; contador < 10; contador++)
+            {
+                Console.WriteLine($"Digite o {contador+1} numero");
+                numeros[contador] = int.Parse(Console.ReadLine());
+            }
+                Console.WriteLine("Numeros que voce cadastrou");
+
+
+                for (var contador = 0; contador < 10; contador++)
+                {
+                    Console.WriteLine(+numeros[contador]);
+                }
+        }
+    }
+}
